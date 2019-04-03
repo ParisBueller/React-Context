3d42dc21bd035ed780b8ee30012ba4cb8a8e9f64
Paris Ward <wardparis.pw@gmail.com>
2019-04-03 09:52:41 -0700

    Context objects and Providers

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import LanguageContext from '../contexts/LanguageContext';
+
 class Button extends React.Component {
+    //this is how we hook up a context object to a class component
+    static contextType = LanguageContext;
+
     render() {
-        return <button className="ui button primary">Submit</button>;
+        const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+        return <button className="ui button primary">{text}</button>;
     }
 }
 
---
+++ src/contexts/LanguageContext.js
@@ -0,0 +1,4 @@
+import React from 'react';
+//we can set a default value with context
+//in this case the string 'english' is the default
+export default React.createContext('english');
